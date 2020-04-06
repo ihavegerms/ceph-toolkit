475bd7987d96434f7fdaf35cdd3a60f1db302e66
alfano93 <david.alfano@rackspace.com>
2020-04-06 14:06:40 -0500

    editing ssd_rotational_fix.sh to work with ansible

--- scripts/ssd_rotational_fix.sh
+++ scripts/ssd_rotational_fix.sh
@@ -3,19 +3,22 @@
 UDEV_RULE_FILE="/etc/udev/rules.d/60-ssd-nonrotational.rules"
 echo "" > /etc/udev/rules.d/60-ssd-nonrotational.rules
 VENDOR="$(dmidecode |grep Vendor |awk '{print $2}')"
+omreport="/opt/dell/srvadmin/bin/omreport"
+ssaccli="/usr/sbin/ssacli"
+
 
 # Dell
 if [ "${VENDOR}" = "Dell" ]
 then
-    for VDISK in $(omreport storage vdisk |awk '/^ID/ {print $3}');
+    for VDISK in $($omreport storage vdisk |awk '/^ID/ {print $3}');
     do
-        PDISKS=$(omreport storage pdisk controller=0 vdisk=${VDISK} |awk -F: '/^ID/ {print $4}' |wc -l);
+        PDISKS=$($omreport storage pdisk controller=0 vdisk=${VDISK} |awk -F: '/^ID/ {print $4}' |wc -l);
         if (( $PDISKS == 1 ))
         then
-            MEDIA=$(omreport storage vdisk controller=0 vdisk=${VDISK} |awk '/^Media/ {print $3}')
+            MEDIA=$($omreport storage vdisk controller=0 vdisk=${VDISK} |awk '/^Media/ {print $3}')
             if [ "${MEDIA}" = "SSD" ]
             then
-                BLKDEV=$(omreport storage vdisk controller=0 vdisk=${VDISK} |awk -F\/ '/^Device Name/ {print $NF}')
+                BLKDEV=$($omreport storage vdisk controller=0 vdisk=${VDISK} |awk -F\/ '/^Device Name/ {print $NF}')
                 echo "# device ${BLKDEV} set rotational for SSD" >> ${UDEV_RULE_FILE}
                 SCSIDEV=$(ls /sys/block/${BLKDEV}/device/scsi_device/)
                 echo "ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNELS==\"${SCSIDEV}\", ATTR{queue/rotational}=\"0\"" >> ${UDEV_RULE_FILE}
@@ -26,21 +29,23 @@ then
 #HP
 elif [ "${VENDOR}" = "HP" ]
 then
-    for ARRAY in $(ssacli ctrl slot=3 array all show | awk '/  Array / {print $2}');
-        do
-            BLKID=$(ssacli ctrl slot=3 array ${ARRAY} ld all show detail | awk '/Disk Name:/ {print $3}'| sed 's/dev\///')
-            for PDISK in $(ssacli ctrl slot=3 array ${ARRAY} pd all show | awk '/physicaldrive/ {print $2}');
+    for slot in $(ssacli ctrl all show config | sed -n '/.*rray.*in Slot/{s/.*in Slot \([^ ]\{1,\}\).*/\1/;p;}'))
+        for ARRAY in $($ssacli ctrl slot=$slot array all show | awk '/  Array / {print $2}');
             do
-                MEDIA=$(ssacli ctrl slot=3 array ${ARRAY} pd all show | grep ${PDISK} | awk '{print $8}' | tr -d /,)
-                if [ "${MEDIA}" = "SSD" ]
-                then
-                    SCSIDEV=$(ls /sys/block/${BLKID}/device/scsi_device/)
-                    echo "# device ${BLKID} set rotational for SSD" >> ${UDEV_RULE_FILE}
-                    echo "ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNELS==\"${SCSIDEV}\", ATTR{queue/rotational}=\"0\"" >> ${UDEV_RULE_FILE}
-                fi
+                BLKID=$($ssacli ctrl slot=$slot array ${ARRAY} ld all show detail | awk '/Disk Name:/ {print $3}'| sed 's/dev\///')
+                for PDISK in $($ssacli ctrl slot=3 array ${ARRAY} pd all show | awk '/physicaldrive/ {print $2}');
+                do
+                    MEDIA=$($ssacli ctrl slot=$slot array ${ARRAY} pd all show | grep ${PDISK} | awk '{print $8}' | tr -d /,)
+                    if [ "${MEDIA}" = "SSD" ]
+                    then
+                        SCSIDEV=$(ls /sys/block/${BLKID}/device/scsi_device/)
+                        echo "# device ${BLKID} set rotational for SSD" >> ${UDEV_RULE_FILE}
+                        echo "ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNELS==\"${SCSIDEV}\", ATTR{queue/rotational}=\"0\"" >> ${UDEV_RULE_FILE}
+                    fi
+                done
             done
         done
-
+    done
 else
 echo "Vendor not supported"
 fi
